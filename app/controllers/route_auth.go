9ae245187d703b660513ee644d32579d84d75df6
kenya6565 <ken.australia0605@gmail.com>
2023-01-01 19:15:09 +0900

    create signup()

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -1,7 +1,29 @@
 package controllers
 
-import "net/http"
+import (
+	"go-todo/app/models"
+	"go-todo/model"
+	"log"
+	"net/http"
+)
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar", "signup")
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		user := model.User{
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+		if err := models.CreateUser(&user); err != nil {
+			log.Println(err) 
+		}
+	}
+	http.Redirect(w, r, "/", 302)
+
 }
